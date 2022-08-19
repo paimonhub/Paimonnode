4d2f70b5605b25641d2543abf685090092f645c0
WilliamXor <williamxor@proton.me>
2022-08-19 17:09:26 +0800

    upgrade fetcher

---
+++ nodefetcher/up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+status_log=$(git status -sb)
+if [ "$status_log" == "## main...origin/main" ];then
+  echo -e "\033[42;30m nothing to commit, working tree clean \033[0m"
+else
+  git status -s && git pull origin main && git add . && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') Upgrade" && git push origin main
+fi
