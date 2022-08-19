c6a0c4dc19b0c55831ce640613b1f558a83a1ee4
WilliamXor <williamxor@proton.me>
2022-08-19 23:10:10 +0800

    upgrade pusher

---
+++ publish/index.js
@@ -0,0 +1,11 @@
+const MongoClient = require('mongodb').MongoClient;
+const fs = require("fs");
+const url = process.env.PAIMONNODE_DB_URI;
+const client = new MongoClient(url);
+(async()=>{
+    await client.connect();
+    let res = await client.db("paimonnode").collection("res").find({"symbol": "pmw"}).toArray()
+    fs.writeFileSync("./clash.yaml",res[0]['sub-clash'])
+    fs.writeFileSync("./base64",res[0]['sub-v2'])
+    process.exit(0)
+})()
